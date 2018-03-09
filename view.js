d119c85260146d2f1c42cde3b71d1e38303be9ae
Chadha Degachi SE2015 <chadha.degachi.2015@uni.strath.ac.uk>
2018-03-09 16:40:29 +0000

    Revert "working brick collision"
    This reverts commit 6b73ce951e02e5ad149bf73d185cce7bf9d5e35e

--- view.js
+++ view.js
@@ -3,15 +3,16 @@ function view() {
         canvas = document.getElementById("canvas"),
         canvasBall,
         canvasPaddle,
+        canvasBrick,
         bricks = [],
         brickRows = 4,
         brickCols = 5,
-        gravity = 0.1,
-        angle = (90 * Math.PI/180),
-        speed = 25,
-        bounce = 0.8,
+        gravity = 0.5,
+        angle = (45 * Math.PI/180),
+        speed = 10,
+        bounce = 0.70 ,
         friction = 0.01,
-        acceleration = 0.5;
+        acceleration = 0.05;
         drawCircle = function (context, x,y,r, fill) {
             context.beginPath();
             context.fillStyle = fill;
@@ -27,41 +28,24 @@ function view() {
             context.closePath();
         },
         canvasPaint =  function () {
-            var context = canvas.getContext("2d"),
-                currentx = 0,
-                currenty = 0;
+            var context = canvas.getContext("2d");
             context.clearRect(0,0, canvas.width, canvas.height);
+            moveBall();
+            drawCircle(context,canvasBall.x, canvasBall.y, canvasBall.r, canvasBall.fillColor);
             drawRectangle(context, canvasPaddle.x, canvasPaddle.y, canvasPaddle.width, canvasPaddle.height , canvasPaddle.fillColor);
+            //drawRectangle(context, canvasBrick.x, canvasBrick.y, canvasBrick.width, canvasBrick.height, canvasBrick.fillColor);
 
             for(var i = 0; i < brickCols; i++) {
-                bricks[i] = [];
                 for(var n = 0; n < brickRows; n++) {
-                    bricks[i][n] = {};
-                    bricks[i][n].fillColor = "#fdf3c3";
-                    bricks[i][n].x = currentx;
-                    bricks[i][n].y = currenty;
-                    bricks[i][n].width = percentage(canvas.width,23);
-                    bricks[i][n].height = percentage(canvas.height, 4);
+                    drawRectangle(context, canvasBrick.x, canvasBrick.y, canvasBrick.width, canvasBrick.height, canvasBrick.fillColor);
 
-                    console.log("paint at col: " + i + " row " + n + " brick x is " + bricks[i][n].x + " y is " + bricks[i][n].y);
-
-                    drawRectangle(context, bricks[i][n].x, bricks[i][n].y, bricks[i][n].width, bricks[i][n].height, bricks[i][n].fillColor);
-                    currentx = currentx + percentage(canvas.width,3) + bricks[i][n].width;
-                    bricks[i][n].x = currentx;
-                    bricks[i][n].y = currenty;
+                    canvasBrick.x = canvasBrick.x + percentage(canvas.width,3) + canvasBrick.width;
                 }
-                currentx = 0;
-                currenty = currenty + percentage(canvas.height,1) + percentage(canvas.height, 4);
+                canvasBrick.x = 0;
+                canvasBrick.y = canvasBrick.y + percentage(canvas.height,1) + canvasBrick.height;
             };
-            currentx = 0;
-            currenty = 0;
-
-            console.log("paint outside loop at col: " + 2 + " row " + 3 + " brick x is " + bricks[2][3].x + " y is " + bricks[2][3].y);
-
-            moveBall();
-
-            drawCircle(context,canvasBall.x, canvasBall.y, canvasBall.r, canvasBall.fillColor);
-
+            canvasBrick.x = 0;
+            canvasBrick.y = 0;
         };
 
     this.movePaddle = function (distance) {
@@ -74,8 +58,17 @@ function view() {
     this.init = function () {
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
-        canvasBall = {fillColor: "pink", x: percentage(canvas.width,50), y: percentage(canvas.height,50) , r: 50, xVelocity: (Math.cos(angle)*speed), yVelocity: (Math.sin(angle)*speed)};
+        canvasBall = {fillColor: "pink", x: percentage(canvas.width,10), y: percentage(canvas.height,15) , r: 50, xVelocity: (Math.cos(angle)*speed), yVelocity: (Math.sin(angle)*speed)};
         canvasPaddle = {fillColor: "#d6e9c6", x: (canvas.width - percentage(canvas.width,70)), y: (canvas.height - percentage(canvas.height,5)), width: percentage(canvas.width,30), height: percentage(canvas.height,15)};
+        canvasBrick = {fillColor: "#fdf3c3", x: 0, y: 0, width: percentage(canvas.width,23),  height: percentage(canvas.height, 4)};
+
+        for(var i = 0; i < brickCols; i++) {
+            bricks[i] = [];
+            for(var n = 0; n < brickRows; n++) {
+                bricks[i][n] = canvasBrick;
+            };
+        };
+
         loop();
     };
 
@@ -86,13 +79,41 @@ function view() {
         canvasBall.x += canvasBall.xVelocity;
         canvasBall.y += canvasBall.yVelocity;
 
-        wallCollision();
-        paddleCollision();
+        if(canvasBall.x + canvasBall.r  >= canvas.width){
+            canvasBall.x = canvas.width - canvasBall.r;
+            canvasBall.xVelocity = - canvasBall.xVelocity;
+            canvasBall.xVelocity *= bounce;
+        }else if(canvasBall.x - canvasBall.r <= 0){
+            canvasBall.x = canvasBall.r;
+            canvasBall.xVelocity = - canvasBall.xVelocity;
+            canvasBall.xVelocity *= bounce;
+        };
 
-        for(var i = 0; i < brickCols; i++) {
-            for(var n = 0; n < brickRows; n++) {
-                console.log("move at col: " + i + " row " + n + " brick x is " + bricks[i][n].x + " y is " + bricks[i][n].y);
-                brickCollision(bricks[i][n]);
+        if(canvasBall.y + canvasBall.r >= canvas.height){
+            canvasBall.y = canvas.height - canvasBall.r;
+            canvasBall.yVelocity = - canvasBall.yVelocity;
+            canvasBall.yVelocity *= bounce;
+
+        }else if(canvasBall.y - canvasBall.r <= 0){
+            canvasBall.y = canvas.r;
+            canvasBall.yVelocity = - canvasBall.yVelocity;
+            canvasBall.yVelocity *= bounce;
+        };
+
+        if(canvasBall.y + canvasBall.r >= canvasPaddle.y){
+            if(canvasBall.x > canvasPaddle.x && canvasBall.x < canvasPaddle.x + canvasPaddle.width){
+                var incomingAngle = Math.atan2(canvasBall.yVelocity, canvasBall.xVelocity),
+                    theta = 180 - incomingAngle;
+                theta = - theta * (Math.PI/180);
+                var cos = Math.cos(theta),
+                    sin = Math.sin(theta),
+                    vy= Math.round(10000*(canvasBall.xVelocity* sin + canvasBall.yVelocity * cos))/10000,
+                    vx = Math.round(10000*(canvasBall.xVelocity * cos - canvasBall.yVelocity * sin))/10000;
+                canvasBall.xVelocity = vx;
+                canvasBall.yVelocity = vy;
+
+                canvasBall.xVelocity += acceleration;
+                canvasBall.yVelocity += acceleration;
             };
         };
     };
@@ -104,50 +125,6 @@ function view() {
 
     function percentage(number, percent) {
         return (number*percent)/100;
-    };
-
-    function wallCollision() {
-        if(canvasBall.x + canvasBall.r  >= canvas.width){
-            canvasBall.x = canvas.width - canvasBall.r;
-            canvasBall.xVelocity = -canvasBall.xVelocity;
-            canvasBall.xVelocity *= bounce;
-        }else if(canvasBall.x - canvasBall.r <= 0){
-            canvasBall.x = canvasBall.r;
-            canvasBall.xVelocity = -canvasBall.xVelocity;
-            canvasBall.xVelocity *= bounce;
-        };
-
-        if(canvasBall.y + canvasBall.r >= canvas.height){
-            canvasBall.y = canvas.height - canvasBall.r;
-            canvasBall.yVelocity = - canvasBall.yVelocity;
-            canvasBall.yVelocity *= bounce;
-
-        }else if(canvasBall.y - canvasBall.r <= 0){
-            canvasBall.y = canvasBall.r;
-            canvasBall.yVelocity = - canvasBall.yVelocity;
-            canvasBall.yVelocity *= bounce;
-        };
-    };
-
-    function paddleCollision() {
-        if(canvasBall.y + canvasBall.r >= canvasPaddle.y){
-            if(canvasBall.x >= canvasPaddle.x && canvasBall.x <= canvasPaddle.x + canvasPaddle.width){
-                canvasBall.yVelocity = - canvasBall.yVelocity;
-                canvasBall.xVelocity += acceleration;
-                canvasBall.yVelocity += acceleration;
-            };
-        };
-    };
-
-    function brickCollision(b) {
-        //console.log("Current brick x: " + b.x);
-        //console.log("Current brick y: " + b.y);
-       // console.log("Current ball x: " + canvasBall.x + " y: " + canvasBall.y);
-        if(canvasBall.x > b.x && canvasBall.x < b.x + b.width && canvasBall.y > b.y && canvasBall.y < b.y + b.height) {
-            console.log("i collided!");
-            canvasBall.yVelocity = -canvasBall.yVelocity;
-            canvasBall.yVelocity *= bounce;
-        };
     };
 
 };
